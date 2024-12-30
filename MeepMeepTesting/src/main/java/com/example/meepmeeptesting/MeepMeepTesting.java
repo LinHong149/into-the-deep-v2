d60babb4c777665c18314e582d64bbf9d9cc1800
LinHong <linlin.hong1@gmail.com>
2024-12-29 20:47:38 -0500

    MeepMeep Pathing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -16,14 +16,21 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .setDimensions(18, 18)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-38, -61, Math.toRadians(90)))
-                        .splineToLinearHeading(new Pose2d(-52, -53,Math.toRadians(45)), Math.toRadians(45)) // Drop off pre-loaded
-                        .splineTo(new Vector2d(-48, -40), Math.toRadians(90)) // To first sample
-                        .splineToLinearHeading(new Pose2d(-52, -53,Math.toRadians(45)), Math.toRadians(45)) // Drop off sample 1
-                        .splineTo(new Vector2d(-53, -40), Math.toRadians(110)) // To second sample
-                        .splineToLinearHeading(new Pose2d(-52, -53,Math.toRadians(45)), Math.toRadians(45)) // Drop off sample 2
-                        .splineTo(new Vector2d(-56, -40), Math.toRadians(130)) // To third sample
-                        .splineToLinearHeading(new Pose2d(-52, -53,Math.toRadians(45)), Math.toRadians(45)) // Drop off sample 3
-                        .splineTo(new Vector2d(-30, -11), Math.toRadians(0)) // To parking
+                        .splineToLinearHeading(new Pose2d(-52, -53, Math.toRadians(45)), Math.toRadians(45)) //drop off pre loaded
+                        .splineTo(new Vector2d(-48,-40),Math.toRadians(90)) //to first sample; rotation 0.5
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-52, -53),Math.toRadians(225)) //drop off sample 1
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-53,-40),Math.toRadians(110)) //to second sample; rotation 0.75
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-52, -53),Math.toRadians(225)) //drop off sample 2
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-56,-40),Math.toRadians(130)) // to third sample; rotation 0.95/1
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-52,-53),Math.toRadians(225)) //drop off sample 3
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-30,-11),Math.toRadians(0))
+
                         .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
